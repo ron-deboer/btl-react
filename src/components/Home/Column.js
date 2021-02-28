9354ec4f1a3e70af8430c204309d65e9a7682656
ron-deboer <ron.deboer@ymail.com>
2021-02-28 18:44:53 +1100

    column to functional

--- src/components/Home/Column.js
+++ src/components/Home/Column.js
@@ -1,31 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Card from './Card';
 
-class Column extends Component {
-    state = {};
-
-    componentDidMount() {}
-
-    render() {
-        return (
-            <div className="column">
-                <div className="header">
-                    <div className="title">{this.props.title} Items</div>
-                    <button className="btn-edit" data-title="Edit" data-toggle="modal" data-target="#edit">
-                        <i className="fa fa-plus fa-xs icon"></i>
-                    </button>
-                </div>
-                <ul className="open-items items">
-                    {this.props.items.map((card, i) => (
-                        <li key={card.id}>
-                            <Card item={card} />
-                        </li>
-                    ))}
-                </ul>
+const Column = (props) => {
+    return (
+        <div className="column">
+            <div className="header">
+                <div className="title">{props.title} Items</div>
+                <button className="btn-edit" data-title="Edit" data-toggle="modal" data-target="#edit">
+                    <i className="fa fa-plus fa-xs icon"></i>
+                </button>
             </div>
-        );
-    }
-}
+            <ul className="open-items items">
+                {props.items.map((card, i) => (
+                    <li key={card.id}>
+                        <Card item={card} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default Column;
