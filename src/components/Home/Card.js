845589e879f39854d9a846d440a1800d42813dcc
Ron DeBoer <77649091+ron-deboer@users.noreply.github.com>
2022-07-05 20:35:34 +1000

    Update Card.js

--- src/components/Home/Card.js
+++ src/components/Home/Card.js
@@ -5,15 +5,9 @@ import MessageBus from '../../_services/Messagebus';
 
 import './card.scss';
 
-class Card extends Component {
+
+const Card = (props) => {
     state = {};
-
-    constructor(props) {
-        super(props);
-        this.handleEditClick = this.handleEditClick.bind(this);
-    }
-
-    componentDidMount() {}
 
     handleEditClick = () => {
         const payload = {
@@ -52,84 +46,83 @@ class Card extends Component {
         return 'badge-success';
     }
 
-    render() {
-        return (
-            <div className="card-container">
-                <div className="item-card-body">
-                    <div className="item-card-header">
-                        <div className="title">
-                            <span className="id">({this.props.item.id})</span> {this.props.item.title}
-                        </div>
-                        <div className="handle">
-                            <button
-                                className="btn-edit"
-                                onClick={(e) => {
-                                    this.handleEditClick();
-                                }}
-                            >
-                                <span className="icon">. . .</span>
-                            </button>
-                        </div>
+    return (
+        <div className="card-container">
+            <div className="item-card-body">
+                <div className="item-card-header">
+                    <div className="title">
+                        <span className="id">({this.props.item.id})</span> {this.props.item.title}
                     </div>
-                    <div className="item-card-text">
-                        <div className="codes">
-                            <div className="code">
-                                Project
-                                <br />
-                                <span className={`badge badge-pill badge-info`}>
-                                    {' '}
-                                    {this.props.item.projectcode}
-                                </span>
-                            </div>
-                            <div className="code">
-                                Priority
-                                <br />
-                                <span
-                                    className={`badge badge-pill ${this.getCodeClass(
-                                        'priority',
-                                        this.props.item.projectcode
-                                    )}`}
-                                >
-                                    {this.props.item.prioritycode}
-                                </span>
-                            </div>
-                            <div className="code">
-                                Size
-                                <br />
-                                <span
-                                    className={`badge badge-pill ${this.getCodeClass(
-                                        'size',
-                                        this.props.item.sizecode
-                                    )}`}
-                                >
-                                    {this.props.item.sizecode}
-                                </span>
-                            </div>
-                            <div className="code">
-                                Status
-                                <br />
-                                <span
-                                    className={`badge badge-pill ${this.getCodeClass(
-                                        'status',
-                                        this.props.item.statuscode
-                                    )}`}
-                                >
-                                    {this.props.item.statuscode}
-                                </span>
-                            </div>
-                            <div className="code">
-                                Assigned
-                                <br />
-                                <span className={`badge badge-pill badge-primary`}>
-                                    {this.props.item.assignedtouser}
-                                </span>
-                            </div>
+                    <div className="handle">
+                        <button
+                            className="btn-edit"
+                            onClick={(e) => {
+                                this.handleEditClick();
+                            }}
+                        >
+                            <span className="icon">. . .</span>
+                        </button>
+                    </div>
+                </div>
+                <div className="item-card-text">
+                    <div className="codes">
+                        <div className="code">
+                            Project
+                            <br />
+                            <span className={`badge badge-pill badge-info`}>
+                                {' '}
+                                {this.props.item.projectcode}
+                            </span>
+                        </div>
+                        <div className="code">
+                            Priority
+                            <br />
+                            <span
+                                className={`badge badge-pill ${this.getCodeClass(
+                                    'priority',
+                                    this.props.item.projectcode
+                                )}`}
+                            >
+                                {this.props.item.prioritycode}
+                            </span>
+                        </div>
+                        <div className="code">
+                            Size
+                            <br />
+                            <span
+                                className={`badge badge-pill ${this.getCodeClass(
+                                    'size',
+                                    this.props.item.sizecode
+                                )}`}
+                            >
+                                {this.props.item.sizecode}
+                            </span>
+                        </div>
+                        <div className="code">
+                            Status
+                            <br />
+                            <span
+                                className={`badge badge-pill ${this.getCodeClass(
+                                    'status',
+                                    this.props.item.statuscode
+                                )}`}
+                            >
+                                {this.props.item.statuscode}
+                            </span>
+                        </div>
+                        <div className="code">
+                            Assigned
+                            <br />
+                            <span className={`badge badge-pill badge-primary`}>
+                                {this.props.item.assignedtouser}
+                            </span>
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
+
 }
 
 export default Card;
